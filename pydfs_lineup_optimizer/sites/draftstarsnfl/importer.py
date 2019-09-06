65cf28b83069916c2eda74dd4b9c143e6731ba53
Antony Papadimitriou <ant.papa22@gmail.com>
2019-09-06 10:13:22 +1000

    add game info

--- pydfs_lineup_optimizer/sites/draftstarsnfl/importer.py
+++ pydfs_lineup_optimizer/sites/draftstarsnfl/importer.py
@@ -1,7 +1,7 @@
 import csv
 from pydfs_lineup_optimizer.exceptions import LineupOptimizerIncorrectCSV
 from pydfs_lineup_optimizer.lineup_importer import CSVImporter
-from pydfs_lineup_optimizer.player import Player
+from pydfs_lineup_optimizer.player import Player, GameInfo
 from pydfs_lineup_optimizer.sites.sites_registry import SitesRegistry
 from pydfs_lineup_optimizer.constants import Site
 
@@ -16,6 +16,13 @@ class DraftstarsCSVImporter(CSVImporter):  # pragma: nocover
         with open(self.filename, 'r') as csvfile:
             csv_data = csv.DictReader(csvfile, skipinitialspace=True)
             for row in csv_data:
+                game_info = None
+                try:
+                    home_team = row["HomeTeam"]
+                    away_team = row["AwayTeam"]
+                    game_info = GameInfo(home_team, away_team, None, False)
+                except ValueError:
+                    pass
                 try:
                     player = Player(
                         row['Player ID'],
@@ -25,7 +32,8 @@ class DraftstarsCSVImporter(CSVImporter):  # pragma: nocover
                         row['Team'],
                         float(row['Price']),
                         float(row['Projection']),
-                        max_exposure=float(row['Max Exposure'])
+                        max_exposure=float(row['Max Exposure']),
+                        game_info=game_info
                     )
                 except KeyError:
                     raise LineupOptimizerIncorrectCSV
